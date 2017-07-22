ae1e25d5adabff0a1ba652f0ed0bbad6eadbee37
Alexander Zubkov <info@zubkov.info>
2017-07-22 17:10:45 +0300

    Version 0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+## 0.2.0
+
+* Added datasets support
+
+
+## 0.1.0
+
+* First release
--- lib/kaminari/sequel/version.rb
+++ lib/kaminari/sequel/version.rb
@@ -2,6 +2,6 @@
 
 module Kaminari
   module Sequel
-    VERSION = '0.1.0'.freeze
+    VERSION = '0.2.0'.freeze
   end
 end
